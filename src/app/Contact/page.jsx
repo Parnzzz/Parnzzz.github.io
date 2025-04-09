91375748b06ee4f483c63460438884ffce92a3a9
Parnzzz <114709549+Parnzzz@users.noreply.github.com>
2025-04-09 16:59:27 +0700

    Update page.jsx

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -6,7 +6,7 @@ function contactPage() {
     <div className='flex'>
       <Header />
       <div className='bg-gray-100 flex flex-col'></div>
-       <div className='text-[80px] flex-col ml-40'> 
+       <div className='bg-gray-100 text-[80px] flex-col ml-40'> 
        <div>Email: 16354.smr@gmail.com</div>
       
        <div>Phone: 0808044850</div>
