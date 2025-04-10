8687a918007ac813cffc166b699f7ed3a124e82f
Parnzzz <114709549+Parnzzz@users.noreply.github.com>
2025-04-10 13:40:07 +0700

    Update page.jsx

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -15,8 +15,8 @@ function aboutpage() {
   return (
     <div className='flex'>
       <Header />
-       <div className='flex bg-gray-100 w-full'>
-       <div className='ml-40'>
+       <div className='min-h-screen flex bg-gray-100 w-full'>
+       <div className='ml-104'>
        <h3>ABOUT ME</h3>
        <h5>  My name is Kittisak buachan . My nickname is TEE .My age is 25 years old . now i'm looking for job developer. 
          My target is work as developer and develop myself for successful workjob in my life . i able to work in bangkok.
