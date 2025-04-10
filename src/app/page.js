1f8343ba68feb18a274aff3b4659424020ae21e5
Parnzzz <114709549+Parnzzz@users.noreply.github.com>
2025-04-10 12:57:50 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ ${slideInLeft}
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div className="flex bg-gradient-to-r from-lime-300 to-lime-600">
       <Header />
       <div className= " bg-gradient-to-r from-lime-300 to-lime-600 w-full min-h-screen flex flex-col items-start justify-center ">
       <StyledComponent> HELLO </StyledComponent>
